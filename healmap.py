ed44f2ea6890f3bc12805d6e8657ec614841b0d3
Reuben Walter Ogburn <rwogburn@gmail.com>
2015-12-10 22:10:02 -0500

    Support loading full coadd map

--- healmap.py
+++ healmap.py
@@ -58,6 +58,10 @@ def get_input_maps(split='D',use_nside=512,use_cal=1.0,dir='.'):
     print "Using 353/30 frequency cross"
     h1 = HealMap('HFI_SkyMap_353_2048_R2.02_full.fits',nside=use_nside,cal=1.0e6,dir=dir)
     h2 = HealMap('HFI_SkyMap_30-1024_R2.01_full.fits',nside=use_nside,cal=1.0e6,dir=dir)
+  elif split == '':
+    print "Using full map, no split"
+    h1 = HealMap('HFI_SkyMap_353_2048_R2.02_full.fits',nside=use_nside,cal=1.0e6,dir=dir)
+    h2 = []
   else:
     raise ValueError("Unrecognized split code "+split+".")
   print "Maps are ready."
